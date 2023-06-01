3f425a4ffb495f76d5c49f4045f3aee885efc240
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-06-01 09:23:46 -0500

    Abstract Screen added

--- mysql_code/oop_db.sql
+++ mysql_code/oop_db.sql
@@ -23,7 +23,7 @@ INSERT INTO all_patterns VALUES
 	('Singleton Design Patterm', 'The Singleton Pattern defines a class that has only one instance a provides a global point of access to it.', 'CREATIONAL'),
 	('Prototype Pattern', 'The Prototype Pattern consists of cloning an existing object instead of creating a new class anc customized with the requirement', 'CREATIONAL'),
 	('Builder Pattern', 'The Builder Design Pattern consists of constructing a complex object from simple objects using an incremental approach.', 'CREATIONAL'),
-	('Object Pool Pattern', 'The Object Pool Pattern says to reuse the object that\'s expensive to create', 'CREATIONAL'),
+	('Object Pool Pattern', "The Object Pool Pattern says to reuse the object that's expensive to create", 'CREATIONAL'),
 	('Adapter Pattern', 'The Adapter Pattern consists of converting the interface of a class to another interface a client wants', 'STRUCTURAL'),
 	('Bridge Pattern', 'The Bridge Pattern says to decouple he functional abstraction from the implementation so that they may vary independently', 'STRUCTURAL'),
 	('Composite Pattern', 'The Composie Pattern consists of allowing clients to operate in a general manner on objects. (May/may not represent a hierachy of objects)', 'STRUCTURAL');
