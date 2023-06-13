9b117933217023bbba39910d24f2b9fd6a52baf3
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-06-13 13:34:46 -0500

    Refactoring

---
+++ database.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector.locales.eng import client_error
+from mysql.connector import Error
+
+import os
+db_pass = os.environ.get('db_pass')
+
+
+class Database():
+   def __init__(self):
+      self.mydb = mysql.connector.connect(
+         host = "127.0.0.1",
+         user = "root",
+         passwd = db_pass,
+         database = "oop_methods"
+      )
+      
+      self.c = self.mydb.cursor()
+      
+   def get_query(self, query):
+      self.c.execute(query)
+      query = self.c.fetchall()
+      query = str(query).replace("(", "").replace(")", "")
+      query = query.replace("[", "").replace("]", "").replace("'", "")
+      return query.rstrip(query[-1])
