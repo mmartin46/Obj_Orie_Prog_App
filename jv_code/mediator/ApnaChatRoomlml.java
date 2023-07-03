1a42c511621c8b1a8cfa24b4924f9251926ab3ac
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-07-03 13:06:36 -0500

    Added Mediator

---
+++ jv_code/mediator/ApnaChatRoomlml.java
@@ -0,0 +1,16 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+// Create a ApnaChatRoomlml class that will implement ApnaChatRoom
+// interface and will also use the # of participants involved in chatting
+// through Participant interface.
+public class ApnaChatRoomlml implements ApnaChatRoom {
+   DateFormat dateFormat = new SimpleDateFormat("E dd-MM-yyyy hh:mm a");
+   Date date = new Date();
+   @Override
+   public void showMsg(String msg, Participant p) {
+      System.out.println(p.getName() + " gets message: " + msg);
+      System.out.println("\t\t\t\t" + "[" + dateFormat.format(date).toString() + "]");
+   }
+}
