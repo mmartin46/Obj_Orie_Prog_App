755fd148e1e18074b94a0c0bc5ac35b7740ca96b
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-07-03 13:58:29 -0500

    Added Memento

---
+++ jv_code/memento/Caretaker.java
@@ -0,0 +1,19 @@
+package memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Create a Caretaker class that will
+// responsible for keeping the Memento.
+public class Caretaker {
+   
+   private List<Memento> mementoList = new ArrayList<Memento>();
+
+   public void add(Memento state) {
+      mementoList.add(state);
+   }
+
+   public Memento get(int index) {
+      return mementoList.get(index);
+   }
+}
