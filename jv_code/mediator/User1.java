ca7f3afcff4cd11eba2e3e95a509beef5462c6ef
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-07-03 13:09:56 -0500

    Added Mediator

---
+++ jv_code/mediator/User1.java
@@ -0,0 +1,28 @@
+/*
+ * Create a User1 class that will extend Participant abstract class
+ * and will use the ApnaChatRoom interface.
+ */
+public class User1 extends Participant {
+   
+   private String name;
+   private ApnaChatRoom chat;
+
+   @Override
+   public void sendMsg(String msg) {
+      chat.showMsg(msg, this);
+   }
+
+   @Override
+   public void setname(String name) {
+      this.name = name;
+   }
+
+   @Override
+   public String getName() {
+      return name;
+   }
+
+   public User1(ApnaChatRoom chat) {
+      this.chat = chat;
+   }
+}
