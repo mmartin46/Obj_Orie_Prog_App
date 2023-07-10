3b7a09aeb8cadb17479a41b4cbbdb6e94c4dd482
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2023-07-10 12:10:24 -0500

    Added Obsevrer

---
+++ jv_code/observer/Observer.java
@@ -0,0 +1,6 @@
+package Obj_Orie_Prog_App.jv_code.observer;
+
+public interface Observer {
+   public void update();
+   public void setSubject(Subject sub);
+}
---
+++ jv_code/observer/Subject.java
@@ -0,0 +1,9 @@
+// https://www.digitalocean.com/community/tutorials/observer-design-pattern-in-java
+public interface Subject {
+   public void register(Observer obj);
+   public void unregister(Observer obj);
+
+   public void notifyObservers();
+
+   public Object getUpdate(Observer obj);
+}
