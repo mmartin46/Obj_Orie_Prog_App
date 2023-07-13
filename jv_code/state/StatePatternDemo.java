fa43e035959e05ee0597bbe4b597a7673ea38187
Mitchell Martin <mmartin46@angelo.edu>
2023-07-13 13:02:26 -0500

    Add files

---
+++ jv_code/state/StatePatternDemo.java
@@ -0,0 +1,22 @@
+public class StatePatternDemo {
+    Controller controller;
+    StatePatternDemo(String con) {
+        controller = new Controller();
+
+        if (con.equalsIgnoreCase("managmenet"))
+            controller.setManagementConnection();
+        if (con.equalsIgnoreCase("sales"))
+            controller.setSalesConnection();
+        if (con.equalsIgnoreCase("accounting"))
+            controller.setAccountingConnection();
+        controller.open();
+        controller.log();
+        controller.close();
+        controller.update();
+    }
+    
+    
+    public static void main(String args[]) {
+        new StatePatternDemo(args[0]);
+    }
+}
